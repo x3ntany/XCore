33e87abf458ca4a5400f9419774e817d9ed3ebc8
x3ntany <xentany@proton.me>
2025-04-28 14:58:22 +0900

    Implement alternative PlayerPointsAPI hooking without service; fallback to direct plugin lookup and API field access since PlayerPoints does not provide a service interface

--- src/main/kotlin/me/xentany/xcore/util/hook/PlayerPointsProvider.kt
+++ src/main/kotlin/me/xentany/xcore/util/hook/PlayerPointsProvider.kt
@@ -2,5 +2,30 @@ package me.xentany.xcore.util.hook
 
 import me.xentany.xcore.util.bukkit.ServiceProvider
 import org.black_ixx.playerpoints.PlayerPoints
+import org.black_ixx.playerpoints.PlayerPointsAPI
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
 
-object PlayerPointsProvider : ServiceProvider<PlayerPoints>(PlayerPoints::class.java)
+
+object PlayerPointsProvider : ServiceProvider<PlayerPointsAPI>(PlayerPointsAPI::class.java) {
+
+  override fun hook(plugin: Plugin): Boolean {
+    val playerPointsEnabled = Bukkit.getPluginManager().isPluginEnabled("PlayerPoints")
+
+    if (playerPointsEnabled) {
+      val api = PlayerPoints.getInstance().api
+
+      if (api != null) {
+        val field = this::class.java.superclass.getDeclaredField("_instance")
+
+        field.isAccessible = true
+        field.set(this, api)
+
+        plugin.slF4JLogger.info("PlayerPointsAPI detected and hooked (direct)")
+        return true
+      }
+    }
+
+    return false
+  }
+}
