89afa9c389e870e34740a9d564ac14d61891b4ac
x3ntany <xentany@proton.me>
2025-04-28 17:07:21 +0900

    Migrate factory

--- src/main/kotlin/me/xentany/xcore/util/time/TimeComponent.kt
+++ src/main/kotlin/me/xentany/xcore/util/time/TimeComponent.kt
@@ -8,4 +8,27 @@ data class TimeComponent(
   val hours: Int,
   val minutes: Int,
   val seconds: Int
-)
+) {
+
+  companion object {
+
+    @JvmStatic
+    fun of(unit: TimeUnit, value: Long): TimeComponent {
+      val totalSeconds = (value * unit.toConvertToSeconds).toLong()
+      val days = totalSeconds / 86400
+      val hours = (totalSeconds / 3600) % 24
+      val minutes = (totalSeconds / 60) % 60
+      val seconds = totalSeconds % 60
+
+      return TimeComponent(
+        totalSeconds = totalSeconds,
+        totalMinutes = totalSeconds / 60,
+        totalHours = totalSeconds / 3600,
+        days = days,
+        hours = hours.toInt(),
+        minutes = minutes.toInt(),
+        seconds = seconds.toInt()
+      )
+    }
+  }
+}
